b34efee4a479686c7fe43b0018b071223f615125
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2015-04-30 09:20:50 +0900

    build: Update install.mk file

--- OSX/install.mk
+++ OSX/install.mk
@@ -2,10 +2,6 @@
 # install.mk
 #
 
-SRCROOT		?= ..
-
-PRODUCT		= KiwiData
-
 install: dummy
 	xcodebuild install -target $(PROJECT_NAME) -project $(PROJECT_NAME).xcodeproj -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO 
 
