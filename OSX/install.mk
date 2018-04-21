0f82b014062ea80854fc3ccba97e1a9a0a188d42
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-04-21 11:01:30 +0900

    build: Remove all Objective-C code

--- OSX/install.mk
+++
@@ -1,9 +0,0 @@
-#
-# install.mk
-#
-
-install: dummy
-	xcodebuild install -target $(PROJECT_NAME) -project $(PROJECT_NAME).xcodeproj -configuration Release DSTROOT=/ ONLY_ACTIVE_ARCH=NO 
-
-dummy:
-
