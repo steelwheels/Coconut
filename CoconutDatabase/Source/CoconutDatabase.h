ad7d66337db01dcfa3253cb7b3e12a3356d01e8d
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-04-22 23:13:37 +0900

    CoconutDatabase: Initial commit of this framework

---
+++ CoconutDatabase/Source/CoconutDatabase.h
@@ -0,0 +1,19 @@
+//
+//  CoconutDatabase.h
+//  CoconutDatabase
+//
+//  Created by Tomoo Hamada on 2018/04/22.
+//  Copyright © 2018年 Steel Wheels Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoconutDatabase.
+FOUNDATION_EXPORT double CoconutDatabaseVersionNumber;
+
+//! Project version string for CoconutDatabase.
+FOUNDATION_EXPORT const unsigned char CoconutDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoconutDatabase/PublicHeader.h>
+
+
