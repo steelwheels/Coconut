c69112a87d54d010267e8b91fbc5b883cf3810de
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2018-11-01 01:45:06 +0900

    CoconutData: Add data type: CNAlignment

---
+++ CoconutData/Source/Graphics/CNGraphics.swift
@@ -0,0 +1,19 @@
+/*
+ * @file	CNGraphics.swift
+ * @brief	Define data type for graphics
+ * @par Copyright
+ *   Copyright (C) 2018 Steel Wheels Project
+ */
+
+import Foundation
+
+public enum CNAlignment: Int32 {
+	case Left
+	case Center
+	case Right
+
+	case Top
+	case Middle
+	case Bottom
+}
+
