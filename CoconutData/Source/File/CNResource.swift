b0556cb180d4c007dca45d5b35d83a1fdab3b254
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-03-02 00:31:13 +0900

    CNResource: Update to have array of strings for each elements

--- CoconutData/Source/File/CNResource.swift
+++ CoconutData/Source/File/CNResource.swift
@@ -11,6 +11,160 @@ private class CNFileResource
 {
 	public typealias LoaderFunc = (_ url: URL) -> Any?
 
+	private var	mPath:		String
+	private var 	mContent:	Any?
+
+	public init(path pathstr: String){
+		mPath		= pathstr
+		mContent	= nil
+	}
+
+	public func load<T>(baseURL url: URL, loader ldr: LoaderFunc) -> T? {
+		if let content = mContent {
+			return content as? T
+		} else {
+			let pathurl = url.appendingPathComponent(mPath)
+			mContent = ldr(pathurl)
+			return mContent as? T
+		}
+	}
+
+	public func toText() -> CNTextLine {
+		return CNTextLine(string: "\"\(mPath)\"")
+	}
+}
+
+private class CNDirectoryResource
+{
+	public typealias LoaderFunc = CNFileResource.LoaderFunc
+
+	private var mFileMap:	Dictionary<String, Array<CNFileResource>>	// Identifier, Array of file resource
+	private var mLoader:	LoaderFunc
+
+	public init(loader ldr: @escaping LoaderFunc){
+		mFileMap	= [:]
+		mLoader		= ldr
+	}
+
+	public func count(identifier ident: String) -> Int? {
+		if let files = mFileMap[ident] {
+			return files.count
+		}
+		return nil
+	}
+
+	public func set(identifier ident: String, path pathstr: String) {
+		mFileMap[ident] = [CNFileResource(path: pathstr)]
+	}
+
+	public func add(identifier ident: String, path pathstr: String) {
+		if let _ = mFileMap[ident] {
+			mFileMap[ident]?.append(CNFileResource(path: pathstr))
+		} else {
+			mFileMap[ident] = [CNFileResource(path: pathstr)]
+		}
+	}
+	
+	public func load<T>(baseURL url: URL, identifier ident: String, index idx: Int) -> T? {
+		if let files = mFileMap[ident] {
+			if idx < files.count {
+				let contents:T? = files[idx].load(baseURL: url, loader: mLoader)
+				return contents
+			}
+		}
+		return nil
+	}
+
+	public func identifiers() -> Array<String> {
+		var result: Array<String> = []
+		for (ident, _) in mFileMap {
+			result.append(ident)
+		}
+		return result
+	}
+
+	public func toText(category cat: String) -> CNTextSection {
+		let section = CNTextSection()
+		section.header = "\(cat): {" ; section.footer = "}"
+
+		for (ident, resources) in mFileMap {
+			let subsec = CNTextSection()
+			subsec.header = "\(ident): {" ; subsec.footer = "}"
+			for resource in resources {
+				let restxt = resource.toText()
+				subsec.add(text: restxt)
+			}
+			section.add(text: subsec)
+		}
+
+		return section
+	}
+}
+
+public class CNResource
+{
+	public typealias LoaderFunc = (_ url: URL) -> Any?
+
+	private var mBaseURL			: URL
+	private var mDirectoryResources		: Dictionary<String, CNDirectoryResource>	// category, directory-resource
+
+	public var baseURL: URL { get { return mBaseURL }}
+
+	public init(baseURL url: URL) {
+		mBaseURL		= url
+		mDirectoryResources	= [:]
+	}
+
+	public func allocate(category cat: String, loader ldr: @escaping LoaderFunc) {
+		mDirectoryResources[cat] = CNDirectoryResource(loader: ldr)
+	}
+
+	public func set(category cat: String, identifier ident: String, path pathstr: String) {
+		mDirectoryResources[cat]?.set(identifier: ident, path: pathstr)
+	}
+
+	public func add(category cat: String, identifier ident: String, path pathstr: String) {
+		mDirectoryResources[cat]?.add(identifier: ident, path: pathstr)
+	}
+
+	public func count(category cat: String, identifier ident: String) -> Int? {
+		return mDirectoryResources[cat]?.count(identifier: ident)
+	}
+
+	public func load<T>(category cat: String, identifier ident: String, index idx: Int) -> T? {
+		let result:T? = mDirectoryResources[cat]?.load(baseURL: mBaseURL, identifier: ident, index: idx)
+		return result
+	}
+
+	public func identifiers(category cat: String) -> Array<String>? {
+		if let resource = mDirectoryResources[cat] {
+			return resource.identifiers()
+		} else {
+			return nil
+		}
+	}
+
+	public func toText() -> CNTextSection {
+		let section = CNTextSection()
+		section.header = "{" ; section.footer = "}"
+
+		let urltxt = CNTextLine(string: "URL: \(mBaseURL.absoluteString)")
+		section.add(text: urltxt)
+
+		for (category, resource) in mDirectoryResources {
+			let subsec = resource.toText(category: category)
+			section.add(text: subsec)
+		}
+		return section
+	}
+}
+
+
+/*
+private class CNFileResource
+{
+	public typealias LoaderFunc = (_ url: URL) -> Any?
+
 	private var	mBaseURL:	URL
 	private var	mLoader:	LoaderFunc
 	private var	mFileMap:	Dictionary<String, String> 	// Identifier, FilePath
@@ -130,4 +284,5 @@ public class CNResource
 		return section
 	}
 }
+*/
 
--- CoconutData/Test/UnitTest/UTResource.swift
+++ CoconutData/Test/UnitTest/UTResource.swift
@@ -11,16 +11,17 @@ import Foundation
 public func testResource(console cons: CNConsole) -> Bool
 {
 	let baseurl  = URL(fileURLWithPath: "/tmp")
-	let resource = CNResource(baseURL: baseurl, console: cons)
+	let resource = CNResource(baseURL: baseurl)
 
-	resource.set(category: "Number", baseURL: baseurl, fileMap: ["number0":"a.num"], loader: {
+	resource.allocate(category: "Number", loader: {
 		(_ url: URL) -> Any? in
 		cons.print(string: "Load resource for \(url.absoluteString)\n")
 		return NSNumber(value: 1.23)
 	})
+	resource.add(category: "Number", identifier: "number0", path: "a.num")
 
 	var result = true
-	if let res: NSNumber = resource.load(category: "Number", identifier: "number0") {
+	if let res: NSNumber = resource.load(category: "Number", identifier: "number0", index: 0) {
 		console.print(string: "[OK] Loaded => \(res.doubleValue)\n")
 	} else {
 		console.print(string: "[Error] Can not load resource0\n")
