e6525f860bafe9409bbe76cd8d00ff708a65caa0
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-07-13 21:17:21 +0900

    CoconutData: Add new method: CNFilePath.schemeInString(string:)

--- CoconutData/Test/UnitTest/UTFilePath.swift
+++ CoconutData/Test/UnitTest/UTFilePath.swift
@@ -10,13 +10,48 @@ import Foundation
 
 public func testFilePath(console cons: CNConsole) -> Bool
 {
+	var result0: Bool
+
+	/* URI */
 	let url = URL(fileURLWithPath: "/System//Library/WidgetResources/AppleClasses/AppleButton.js")
 	if let uti = CNFilePath.UTIForFile(URL: url) {
 		cons.print(string: "UTI: \(uti) \n")
-		return true
+		result0 = true
 	} else {
 		cons.print(string: "Error: No UTI\n")
-		return false
+		result0 = false
 	}
+
+	/* Scheme */
+	let result10 = testScemeInString(string: "/usr/local/bin", expectedResult: false, console: cons)
+	let result11 = testScemeInString(string: "https://yahoo.com", expectedResult: true, console: cons)
+	let result12 = testScemeInString(string: "https//yahoo.com", expectedResult: false, console: cons)
+	let result13 = testScemeInString(string: "ftp.c://yahoo.com", expectedResult: true, console: cons)
+
+	return result0 && result10 && result11 && result12 && result13
 }
 
+private func testScemeInString(string str: String, expectedResult eval: Bool, console cons: CNConsole) -> Bool {
+	var result: Bool
+	cons.print(string: "Get sceme from \"\(str)\" -> ")
+	if let resstr = CNFilePath.schemeInString(string: str) {
+		cons.print(string: "Sceme:\"\(resstr)\" -> ")
+		if eval {
+			cons.print(string: "OK\n")
+			result = true
+		} else {
+			cons.print(string: "NG\n")
+			result = false
+		}
+	} else {
+		cons.print(string: "<none> -> ")
+		if eval {
+			cons.print(string: "NG\n")
+			result = false
+		} else {
+			cons.print(string: "OK\n")
+			result = true
+		}
+	}
+	return result
+}
