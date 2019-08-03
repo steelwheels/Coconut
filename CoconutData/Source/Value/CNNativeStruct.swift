c4e8c8ea59b0eea2724523e0d4da56c7461ebbee
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-08-03 09:29:34 +0900

    CoconutData: Add CNNativeStruct class

---
+++ CoconutData/Source/Value/CNNativeStruct.swift
@@ -0,0 +1,39 @@
+/*
+ * @file	CNNativeStruct.swift
+ * @brief	Define CNNativeStruct class
+ * @par Copyright
+ *   Copyright (C) 2019 Steel Wheels Project
+ */
+
+import Foundation
+
+open class CNNativeStruct
+{
+	private var mStructName:	String
+	private var mMembers:		Dictionary<String, CNNativeValue>
+
+	public init(name nm: String){
+		mStructName = nm
+		mMembers    = [:]
+	}
+
+	public var name: String { return mStructName }
+	public var members: Dictionary<String, CNNativeValue> { return mMembers }
+
+	public func member(name nm: String) -> CNNativeValue? {
+		return mMembers[nm]
+	}
+
+	public func setMember(name nm: String, value val: CNNativeValue){
+		mMembers[nm] = val
+	}
+
+	public func toValue() -> CNNativeValue {
+		var result: Dictionary<String, CNNativeValue> = [:]
+		for name in mMembers.keys {
+			result[name] = mMembers[name]
+		}
+		return CNNativeValue.dictionaryValue(result)
+	}
+}
+
