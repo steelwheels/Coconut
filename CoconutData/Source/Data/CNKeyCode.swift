93bb5c1f06ac5047c4f6dd18f42f35b2e26873a0
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-08-14 09:30:18 +0900

    CoconutData: Add key code definition as CNKeyCode

---
+++ CoconutData/Source/Data/CNKeyCode.swift
@@ -0,0 +1,20 @@
+/**
+ * @file	CNKeyCode.swift
+ * @brief	Define CNKeyCode type
+ * @par Copyright
+ *   Copyright (C) 2019 Steel Wheels Project
+ */
+
+import Foundation
+
+/**
+ * reference: https://qiita.com/baba163/items/e2390c4529ec0448151d
+ */
+public enum CNKeyCode: Int
+{
+	case LeftArrow		= 123
+	case RightArrow		= 124
+	case UpArrow		= 126
+	case DownArrow		= 125
+}
+
