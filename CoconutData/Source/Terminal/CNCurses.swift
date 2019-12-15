ddeca617e040b83ef13d1d948c9587e9445d19e4
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-12-15 22:33:20 +0900

    CoconutData: Update CNCurses class

--- CoconutData/Source/Terminal/CNCurses.swift
+++ CoconutData/Source/Terminal/CNCurses.swift
@@ -18,14 +18,60 @@ public class CNCurses
 		}
 	}
 
+	private var 	mConsole:	CNFileConsole
 	private var	mTerminalInfo:	CNTerminalInfo
 
-	public init(terminalInfo tinfo: CNTerminalInfo) {
+	public init(console cons: CNFileConsole, terminalInfo tinfo: CNTerminalInfo) {
+		mConsole	= cons
 		mTerminalInfo	= tinfo
 	}
 
 	public var size: Size {
 		return Size(width: mTerminalInfo.width, height: mTerminalInfo.height)
+	}
+
+	public func setup(){
+		/* Set raw mode */
+		let _ = mConsole.inputHandle.setRawMode(enable: true)
+
+		/* Send setup requests */
+		let reqcmd = CNEscapeCode.requestScreenSize
+		mConsole.print(string: reqcmd.encode())
+
+		/* Receive ack commands */
+		var didfinish = false
+		while !didfinish {
+			if let str = mConsole.scan() {
+				switch CNEscapeCode.decode(string: str) {
+				case .ok(let codes):
+					for code in codes {
+						receiveSetup(escapeCode: code)
+					}
+					didfinish = didFinishSetup()
+				case .error(let err):
+					mConsole.error(string: "[Error] Failed to setup curses: \(err.description())")
+				}
+			}
+		}
+	}
+
+	public func finalize() {
+		/* Reset raw mode */
+		let _ = mConsole.inputHandle.setRawMode(enable: false)
+	}
+
+	private func receiveSetup(escapeCode code: CNEscapeCode) {
+		switch code {
+		case .screenSize(let width, let height):
+			mTerminalInfo.width	= width
+			mTerminalInfo.height	= height
+		default:
+			mConsole.error(string: "Unrecogized command: \(code.description())")
+		}
+	}
+
+	private func didFinishSetup() -> Bool {
+		return mTerminalInfo.width > 0
 	}
 }
 
