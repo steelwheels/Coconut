5b9270d563020dced0c71d735f56099a54427b65
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2020-05-26 21:34:19 +0900

    CoconutData: Add inkey() method to CNCurses class

--- CoconutData/Source/File/CNCurses.swift
+++ CoconutData/Source/File/CNCurses.swift
@@ -10,11 +10,17 @@ import Foundation
 public class CNCurses
 {
 	private var mTerminalInfo:	CNTerminalInfo
-	private var mConsole:		CNConsole
+	private var mConsole:		CNFileConsole
+	private var mHandler:		((_ hdl: FileHandle) -> Void)?
+	private var mBuffer:		String
+	private var mLock:		NSLock
 
-	public init(console cons: CNConsole, terminalInfo terminfo: CNTerminalInfo) {
+	public init(console cons: CNFileConsole, terminalInfo terminfo: CNTerminalInfo) {
 		mTerminalInfo	= terminfo
 		mConsole	= cons
+		mHandler	= nil
+		mBuffer		= ""
+		mLock		= NSLock()
 	}
 
 	public var width:  Int { get { return mTerminalInfo.width	}}
@@ -28,11 +34,25 @@ public class CNCurses
 		/* Erace buffer */
 		let erace = CNEscapeCode.eraceEntireBuffer
 		mConsole.print(string: erace.encode())
+
+		/* Replace handler */
+		mHandler = mConsole.inputHandle.readabilityHandler
+		mConsole.inputHandle.readabilityHandler = {
+			(_ hdl: FileHandle) -> Void in
+			self.mLock.lock()
+			if let str = String(data: hdl.availableData, encoding: .utf8) {
+				self.mBuffer.append(str)
+			}
+			self.mLock.unlock()
+		}
 	}
 
 	public func end() {
 		let code = CNEscapeCode.selectAltScreen(false)
 		mConsole.print(string: code.encode())
+
+		/* Restore handler */
+		mConsole.inputHandle.readabilityHandler = mHandler
 	}
 
 	public func moveTo(x xpos: Int, y ypos: Int) {
@@ -40,5 +60,19 @@ public class CNCurses
 		let y    = max(0, min(ypos, mTerminalInfo.height - 1))
 		let code = CNEscapeCode.cursorPoisition(y, x)
 		mConsole.print(string: code.encode())
+	}
+
+	public func inkey() -> Character? {
+		let result: Character?
+		mLock.lock()
+		if let c = mBuffer.first {
+			mBuffer.removeFirst()
+			result = c
+		} else {
+			Thread.sleep(forTimeInterval: 0.1)
+			result = nil
+		}
+		mLock.unlock()
+		return result
 	}
 }
