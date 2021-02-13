1eb50fb5b9801edee05518c333c9e26dd2ee132e
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2021-02-13 23:18:44 +0900

    CoconutData: Add init method to CNBitmapData class

--- CoconutData/Source/Graphics/CNBitmapData.swift
+++ CoconutData/Source/Graphics/CNBitmapData.swift
@@ -33,6 +33,40 @@ public class CNBitmapData
 		}
 	}
 
+	public init(intData idata: Array<Array<Int>>) {
+		mPositionX		= 0
+		mPositionY		= 0
+		mHeight			= idata.count
+		var width: Int = 0
+		for row in idata {
+			width = max(width, row.count)
+		}
+		mWidth			= width
+		/* Allocate destination array */
+		mData = []
+		for _ in 0..<mHeight {
+			let row = Array(repeating: CNColor.clear, count: mWidth)
+			mData.append(row)
+		}
+		/* Copy array */
+		for y in 0..<idata.count {
+			let row = idata[y]
+			for x in 0..<row.count {
+				let ival = row[x]
+				mData[y][x] = CNBitmapData.intToColor(intValue: ival)
+			}
+		}
+	}
+
+	private static func intToColor(intValue ival: Int) -> CNColor {
+		if let col = CNColor.color(withEscapeCode: Int32(ival)) {
+			return col
+		} else {
+			NSLog("Unknown color value: \(ival)")
+			return CNColor.clear
+		}
+	}
+
 	public func set(x posx: Int, y posy: Int, color col: CNColor) {
 		if 0<=posx && posx<mWidth {
 			if 0<=posy && posy<mHeight {
